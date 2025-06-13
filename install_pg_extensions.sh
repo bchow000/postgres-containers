5b035b6df8ccecaf88bd9bc137ef9e1a3cff7657
bchow <10820756+bchow000@users.noreply.github.com>
2025-06-12 18:22:57 -0700

    Update install_pg_extensions.sh check for vchord and get package

--- install_pg_extensions.sh
+++ install_pg_extensions.sh
@@ -36,16 +36,37 @@ for EXTENSION in ${EXTENSIONS}; do
         continue
     fi
 
-    if [ $(pgxn search ${EXTENSION} | wc -l) -gt 0 ]; then
-        pgxn install --yes ${EXTENSION}
+    if [ "$EXTENSION" == "vchord" ]; then
+        # Install JQ
+        apt install -y jq
+        # Get the actual latest release tag (follow redirect)
+        latest_tag=$(curl -sL -o /dev/null -w '%{url_effective}' https://github.com/tensorchord/VectorChord/releases/latest | awk -F/ '{print $NF}')
+
+        # List all assets for that release
+        assets_json=$(curl -s https://api.github.com/repos/tensorchord/VectorChord/releases/tags/$latest_tag)
+
+        # Extract the .deb URL for PostgreSQL 17 and x86_64
+        # deb_url=$(echo "$assets_json" | jq -r '.assets[] | select(.name | test("vchord-pg${PG_MAJOR}_.*_amd64.deb")) | .browser_download_url')
+        deb_url=$(echo "$assets_json" | jq -r \
+          --arg pg_major "$PG_MAJOR" \
+          '.assets[] | select(.name | test("vchord-pg" + $pg_major + "_.*_amd64.deb")) | .browser_download_url')
+
+        apt install ${deb_url}
+        
+        existing=$(sudo -u postgres psql -Atc "SHOW shared_preload_libraries;")
+        if [[ "$existing" != *vchord.so* ]]; then
+            combined=$(echo "$existing" | sed 's/^$//' | awk -v add="vchord.so" '{ if ($0 == "") print add; else print $0","add }')
+            sudo -u postgres psql -c "ALTER SYSTEM SET shared_preload_libraries = '$combined';"
+        fi
         continue
     fi
+
     # is it an extension found in apt?
-    # if apt-cache show "postgresql-${PG_MAJOR}-${EXTENSION}" &> /dev/null; then
-        # install the extension
-        # apt-get install -y "postgresql-${PG_MAJOR}-${EXTENSION}"
-    #    continue
-    #fi
+    if apt-cache show "postgresql-${PG_MAJOR}-${EXTENSION}" &> /dev/null; then
+        install the extension
+        apt-get install -y "postgresql-${PG_MAJOR}-${EXTENSION}"
+       continue
+    fi
 
     # extension not found/supported
     echo "Extension '${EXTENSION}' not found/supported"
